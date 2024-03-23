b6c0b2b119da17fee9ca2793d6e224b849e6375b
ravichandran22 <ravichandran220799@gmail.com>
2024-03-23 17:10:07 +0530

    Update: Committing changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -4,11 +4,11 @@ const header = {
 }
 
 const about = {
-  name: 'Ravichandran',
+  name: 'Ravichandran R',
   role: 'Web Developer',
   description:
     'A Software Developer with two years of Experience, specializing in Front-end Technologies',
-  resume: 'https://drive.google.com/file/d/1JDYoQeD57LIkcHzv1tdXeHCHAejl0qmf/view?usp=sharing',
+  resume: 'https://drive.google.com/file/d/1agHBK9gxOcj-pLzsH-yNh6pwKdo3FZhd/view?usp=drive_link',
   social: {
     linkedin: 'https://www.linkedin.com/in/ravi-chandran-r/',
     github: 'https://github.com/ravichandran22',
@@ -16,6 +16,43 @@ const about = {
 }
 
 const projects = [
+  {
+    name: 'QR_Code_Generator_React',
+    description:
+      'Creating a simple QR Code Generator app ReactJs',
+    stack: ['ReactJs'],
+    sourceCode: 'https://github.com/ravichandran22/QR_Code_Generator_React',
+    livePreview: 'https://ravichandran22.github.io/QR_Code_Generator_React/',
+  },
+  {
+    name: 'CRUD_App_React',
+    description:
+      'The CRUD app using React.js is a web application that allows users to perform basic operations on data, including creating, reading, updating, and deleting records.',
+    stack: ['ReactJs'],
+    sourceCode: 'https://github.com/ravichandran22/CRUD_App_React',
+  },
+  {
+    name: 'Simple-CRUD-React-Node-MySQL',
+    description:
+      'It’s a full-stack web application developed using React.js for the front end, Node.js for the back end, and MySQL for the database. The project implements basic CRUD functionality, allowing users to perform operations such as creating, viewing, updating, and deleting data records.',
+    stack: ['ReactJs, NodeJs, MySQL'],
+    sourceCode: 'https://github.com/ravichandran22/Simple-CRUD-React-Node-MySQL',
+  },
+  {
+    name: 'crud-app-with-nodejs-graphql-mongodb',
+    description:
+      'Creating a simple CRUD app with NodeJS, MongoDB, GraphQL',
+    stack: ['ReactJs, NodeJs, MongoDB, GraphQL'],
+    sourceCode: 'https://github.com/ravichandran22/crud-app-with-nodejs-graphql-mongodb',
+  },
+  {
+    name: 'React-UI',
+    description:
+      'A simple React UI application  with a focus on optimizing component reusability and implementing efficient state management resulting in a 20% increase in user interaction and seamless data handling.',
+    stack: ['ReactJs'],
+    sourceCode: 'https://ravichandran22.github.io/guires_task/',
+    livePreview: 'https://ravichandran22.github.io/guires_task/',
+  },
   {
     name: 'Quiz',
     description:
@@ -43,15 +80,16 @@ const projects = [
 ]
 
 const skills = [
+  'ReactJs',
+  'NodeJs',
+  'MySQL',
+  'MongoDB',
   'HTML',
   'CSS',
   'Bootstrap',
   'Material UI',
   'JavaScript',
-  'jQuery',
-  'UI',
-  'React',
-  'Wordpress',
+  'WordPress',
   'Git'
 ]
 
